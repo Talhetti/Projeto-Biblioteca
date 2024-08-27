807766d9e6c5aceac2935fd89ab74d4a008d3e1c
MarceloLuan <marcelobluan@gmail.com>
2024-08-27 19:25:48 -0300

    BarraPesquisa

--- main.js
+++ main.js
@@ -30,3 +30,18 @@ function removeMostrarTentar(section) {
         recarregarPag();
     }
 }
+
+function search_books() {
+    let input = document.getElementById('searchbar').value
+    input=input.toLowerCase();
+    let x = document.getElementsByClassName('books');
+
+    for (i = 0; i < x.length; i++) { 
+        if (!x[i].innerHTML.toLowerCase().includes(input)) {
+            x[i].style.display="none";
+        }
+        else {
+            x[i].style.display="list-item";                 
+        }
+    }
+}
